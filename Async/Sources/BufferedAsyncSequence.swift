0277925da67f56a033f1e5358bfd3a3d1c69324f
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-04-30 18:33:13 +0900

    fix

--- Async/Sources/BufferedAsyncSequence.swift
+++ Async/Sources/BufferedAsyncSequence.swift
@@ -4,23 +4,93 @@
 //
 //  Created by Kota on 4/9/R7.
 //
+extension AsyncSequence where Self: Sendable, Element: Sendable, Failure == Never {
+	public func buffer(count: Int) -> some AsyncSequence<ArraySlice<Element>, Failure> {
+		AsyncStream { future in
+			Task<Void, Never> {
+				var buffer = ArraySlice<Element>()
+				for await value in self {
+					buffer.append(value)
+					switch buffer.prefix(count) {
+					case let prefix where prefix.count == count:
+						defer {
+							buffer.removeFirst(prefix.count)
+						}
+						future.yield(prefix)
+					default:
+						continue
+					}
+				}
+				while !buffer.isEmpty {
+					let prefix = buffer.prefix(count)
+					defer {
+						buffer.removeFirst(prefix.count)
+					}
+					future.yield(prefix)
+				}
+				future.finish()
+			}
+		}
+	}
+}
+extension AsyncSequence where Self: Sendable, Element: Sendable {
+	public func buffer(count: Int) -> some AsyncSequence<ArraySlice<Element>, any Error> {
+		AsyncThrowingStream { future in
+			Task<Void, Never> {
+				do {
+					var buffer = ArraySlice<Element>()
+					for try await value in self {
+						buffer.append(value)
+						switch buffer.prefix(count) {
+						case let prefix where prefix.count == count:
+							defer {
+								buffer.removeFirst(prefix.count)
+							}
+							future.yield(prefix)
+						default:
+							continue
+						}
+					}
+					while !buffer.isEmpty {
+						let prefix = buffer.prefix(count)
+						defer {
+							buffer.removeFirst(prefix.count)
+						}
+						future.yield(prefix)
+					}
+					future.finish()
+				} catch {
+					future.finish(throwing: error)
+				}
+			}
+		}
+	}
+}
 extension AsyncSequence where Self: Sendable, Element: Collection, Element.Element: Sendable, Failure == Never {
 	public func resize(count: Int) -> some AsyncSequence<ArraySlice<Element.Element>, Failure> {
 		AsyncStream { future in
 			Task<Void, Never> {
-				var buffer = Array<Element.Element>()
+				var buffer = ArraySlice<Element.Element>()
 				for await values in self where !values.isEmpty {
 					buffer.append(contentsOf: values)
 					switch buffer.prefix(count) {
 					case let result where result.count == count:
 						defer {
-							buffer.removeFirst(count)
+							buffer.removeFirst(result.count)
 						}
 						future.yield(result)
 					default:
 						continue
 					}
 				}
+				while !buffer.isEmpty {
+					let prefix = buffer.prefix(count)
+					defer {
+						buffer.removeFirst(prefix.count)
+					}
+					future.yield(prefix)
+				}
+				future.finish()
 			}
 		}
 	}
@@ -30,19 +100,27 @@ extension AsyncSequence where Self: Sendable, Element: Collection, Element.Eleme
 		AsyncThrowingStream { future in
 			Task<Void, Never> {
 				do {
-					var buffer = Array<Element.Element>()
+					var buffer = ArraySlice<Element.Element>()
 					for try await values in self where !values.isEmpty {
 						buffer.append(contentsOf: values)
 						switch buffer.prefix(count) {
 						case let result where result.count == count:
 							defer {
-								buffer.removeFirst(count)
+								buffer.removeFirst(result.count)
 							}
 							future.yield(result)
 						default:
 							continue
 						}
 					}
+					while !buffer.isEmpty {
+						let prefix = buffer.prefix(count)
+						defer {
+							buffer.removeFirst(prefix.count)
+						}
+						future.yield(prefix)
+					}
+					future.finish()
 				} catch {
 					future.finish(throwing: error)
 				}
--- Async/Tests/AsyncMap.swift
+++ Async/Tests/AsyncMap.swift
@@ -7,4 +7,17 @@
 import Testing
 @testable import Async
 @Suite
-struct AsyncMapTests {}
+struct AsyncMapTests {
+	@Test
+	func chunk() async {
+		let seq = AsyncStream { future in
+			for val in 0...100 {
+				future.yield(val)
+			}
+			future.finish()
+		}
+		for await seg in seq.buffer(count: 10) {
+			print(seg)
+		}
+	}
+}
