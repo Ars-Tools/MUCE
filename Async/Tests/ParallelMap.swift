79b76852e5a43585cc81b6f40dba43d73e2102e8
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-04-30 18:57:39 +0900

    tests

--- Async/Tests/ParallelMap.swift
+++ Async/Tests/ParallelMap.swift
@@ -5,3 +5,17 @@
 //  Created by Kota on 4/6/R7.
 //
 import Testing
+import typealias Foundation.Thread
+@testable import Async
+@Suite
+struct ParallelMap {
+	@Test(.timeLimit(.minutes(1)))
+	func map() {
+		let source = 0..<12
+		let target = source.parallelMap {
+			Thread.sleep(forTimeInterval: .random(in: 6.0 ... 8.0))
+			return $0 * $0
+		}
+		#expect(target == source.map { $0 * $0 })
+	}
+}
