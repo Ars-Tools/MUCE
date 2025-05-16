013ea55c6a4e13ec9479ff7e023f5b959e8d08c9
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2025-05-16 10:41:52 +0900

    change rawtype

--- OSC/Sources/MIDIMessage.swift
+++ OSC/Sources/MIDIMessage.swift
@@ -5,41 +5,42 @@
 //  Created by Kota on 4/1/R7.
 //
 import typealias CoreMIDI.MIDIMessage_32
+import protocol Synchronization.AtomicRepresentable
 @dynamicMemberLookup
-public struct MIDIMessage: RawRepresentable & Sendable & BitwiseCopyable & Codable {
-	public typealias RawValue = SIMD4<UInt8>
+@frozen public struct MIDIMessage: RawRepresentable & Sendable & BitwiseCopyable & Codable {
+	public typealias RawValue = MIDIMessage_32
 	public var rawValue: RawValue
 	public init(rawValue: RawValue) {
 		assert(MemoryLayout<RawValue>.size == 4)
 		self.rawValue = rawValue
 	}
 }
-extension MIDIMessage {
-	public var port: UInt8 {
-		_read {
-			yield rawValue.x
-		}
-		_modify {
-			yield &rawValue.x
-		}
-	}
-	public var status: UInt8 {
-		_read {
-			yield rawValue.y
-		}
-		_modify {
-			yield &rawValue.y
-		}
-	}
-	public var data: SIMD2<RawValue.Scalar> {
-		_read {
-			yield rawValue.highHalf
-		}
-		_modify {
-			yield &rawValue.highHalf
-		}
-	}
-}
+//extension MIDIMessage {
+//	public var port: UInt8 {
+//		_read {
+//			yield rawValue.x
+//		}
+//		_modify {
+//			yield &rawValue.x
+//		}
+//	}
+//	public var status: UInt8 {
+//		_read {
+//			yield rawValue.y
+//		}
+//		_modify {
+//			yield &rawValue.y
+//		}
+//	}
+//	public var data: SIMD2<RawValue.Scalar> {
+//		_read {
+//			yield rawValue.highHalf
+//		}
+//		_modify {
+//			yield &rawValue.highHalf
+//		}
+//	}
+//}
 extension MIDIMessage {
 	public subscript<R>(dynamicMember keyPath: KeyPath<RawValue, R>) -> R {
 		_read {
@@ -55,3 +56,4 @@ extension MIDIMessage {
 		}
 	}
 }
+extension MIDIMessage: AtomicRepresentable {}
