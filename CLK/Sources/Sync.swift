d6db19f14ac0f820135a327003364d211a38d4ee
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2026-08-03 20:07:30 +0900

    add log

--- CLK/Sources/Sync.swift
+++ CLK/Sources/Sync.swift
@@ -22,19 +22,25 @@ public protocol Synchronisable: SynchroniseSource {
 	func set(rate: Float64, time anchor: CMTime, from reference: CMTime) throws
 }
 extension SynchroniseSource {
-	public func`export`(to endpoint: some IPEndpoint, on queue: Optional<DispatchQueue> = .none) async throws {
-		try await export(by: .init(on: endpoint), on: queue).await
+    public func`export`(to endpoint: some IPEndpoint, on queue: Optional<DispatchQueue> = .none, subsystem: Optional<OSLog> = .none) async throws {
+		try await export(by: .init(on: endpoint), on: queue, subsystem: subsystem).await
 	}
-	public func`export`(by socket: Udp.Socket<some IPEndpoint>, on queue: Optional<DispatchQueue> = .none) -> some AsyncSequence<(), any Error> {
-		socket.send(stream: socket.recv(on: queue).compactMap { (packet, endpoint) in
+    public func`export`(by socket: Udp.Socket<some IPEndpoint>, on queue: Optional<DispatchQueue> = .none, subsystem: Optional<OSLog> = .some(.init(subsystem: "CLK.Export", category: .dynamicTracing))) -> some AsyncSequence<(), any Error> {
+		socket.send(stream: socket.recv(on: queue).compactMap { request, endpoint in
+            if let subsystem {
+                os_log(.debug, log: subsystem, "request: %{public}@ from %{public}@", String(describing: request), String(describing: endpoint))
+            }
 			let response = Array<UInt8>(unsafeUninitializedCapacity: MemoryLayout<CMTime>.stride * 4) {
 				assert($0.count == MemoryLayout<CMTime>.stride * 4)
-				guard $0.initialize(fromContentsOf: packet) == MemoryLayout<CMTime>.stride * 2 else { return }
+				guard $0.initialize(fromContentsOf: request) == MemoryLayout<CMTime>.stride * 2 else { return }
 				$0.withMemoryRebound(to: CMTime.self) {
 					($0[2], $0[3]) = (sign, time)
 				}
 				$1 = MemoryLayout<CMTime>.stride * 4
 			}
+            if let subsystem {
+                os_log(.debug, log: subsystem, "response: %{public}@ from %{public}@", String(describing: response), String(describing: endpoint))
+            }
 			return response.count == MemoryLayout<CMTime>.stride * 4 ?
 				.some((response, endpoint)) :
 				.none
@@ -42,20 +48,25 @@ extension SynchroniseSource {
 	}
 }
 extension Synchronisable {
-	public func`import`<Endpoint: IPEndpoint>(from endpoint: Endpoint, on queue: Optional<DispatchQueue> = .none) async throws {
+    public func`import`<Endpoint: IPEndpoint>(from endpoint: Endpoint, on queue: Optional<DispatchQueue> = .none, subsystem: Optional<OSLog> = .none) async throws {
 		let socket = try Udp.Socket<Endpoint>()
 		let notify = try CMTimebase(sourceClock: .hostTimeClock)
 		try notify.set(rate: 1)
 		let`import` = Task {
 			var anchor = (p: CMTime.invalid, t: CMTime.invalid, τ: CMTime.invalid, ε: CMTime.invalid)
 			for try await (packet, response) in socket.recv(on: queue) where (packet.count, response) == (MemoryLayout<CMTime>.stride * 4, endpoint) {
+                if let subsystem {
+                    os_log(.debug, log: subsystem, "notify: %{public}@ from %{public}@", String(describing: packet), String(describing: response))
+                }
 				let (n, r) = (time, base)
 				let (σ, s, p, τ) = packet.withUnsafeBytes { $0.loadUnaligned(as: (CMTime, CMTime, CMTime, CMTime).self) }
 				let t = CMTimeMultiplyByRatio(CMTimeAdd(r, s), multiplier: 1, divisor: 2)
 				let ε = CMTimeMultiplyByRatio(CMTimeSubtract(r, s), multiplier: 1, divisor: 2)
 				let χ = CMTimeSubtract(n, CMTimeMultiplyByFloat64(ε, multiplier: rate))
 				if σ != sign { // CMTimeCompare(σ, sign)
-					break
+                    if let subsystem {
+                        os_log(.debug, log: subsystem, "NA")
+                    }
 				} else if p != anchor.p {
 					anchor.ε = .positiveInfinity
 					anchor.p = p
@@ -69,10 +80,10 @@ extension Synchronisable {
 					let rate = dτ.seconds / dt.seconds
 					try set(rate: rate, time: τ, from: t)
 				} else {
-					let Δτ = τ - χ
+//					let Δτ = τ - χ
 					let dτ = τ - anchor.τ
 					let dt = t - anchor.t
-					let rate = dτ.seconds / dt.seconds + Δτ.seconds * anchor.ε.seconds / ε.seconds
+					let rate = dτ.seconds / dt.seconds// + Δτ.seconds * anchor.ε.seconds / ε.seconds
 					try set(rate: rate)
 				}
 			}
